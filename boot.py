add3581e58ee4875a66e5d95201556d957c5de26
ravinu <ravinu.gcom@gmail.com>
2022-08-29 07:20:01 +0530

    add else and change var name

--- boot.py
+++ boot.py
@@ -1,9 +1,10 @@
 #boot Sequence
-import imp
 import time
 def boot():
  print("Initiating bootloader")
  time.sleep(0.3)
- contbt = input("Boot into pyOS 20.69? (Y)es/(N)o")
- if contbt == "Y":
-     print("Initiating boot sequence")
+ cbt = input("Boot into pyOS 20.69? (Y)es/(N)o: ")
+ if cbt == "Y":
+    print("Initiating boot sequence")
+ else:
+    exit()
