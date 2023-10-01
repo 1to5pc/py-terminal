ce690d0cacf19daf6647b402a8e36e5d3e409fc8
ravinu <ravinu.gcom@gmail.com>
2023-10-01 13:42:56 +0530

    Convert input to capital

--- boot.py
+++ boot.py
@@ -5,7 +5,8 @@ def boot():
  time.sleep(0.3)
  cbt = input("Boot into pyOS 20.69? [Y/n]: ")
  cbt=cbt.upper()
- if cbt == "Y":
+ cbt = cbt.upper()
+ if cbt == "Y" or "YES":
     print("Initiating boot sequence")
  else:
     print("Aborting boot")
