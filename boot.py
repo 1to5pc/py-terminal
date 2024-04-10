1122137fb921f4fad774c843565c7f6d6c026948
Ravinu H <80444139+1to5pc@users.noreply.github.com>
2024-04-10 17:41:26 +0530

    Increase cohesiveness

--- boot.py
+++ boot.py
@@ -3,7 +3,7 @@ import time
 def boot():
  print("Initiating bootloader")
  time.sleep(0.3)
- cbt = input("Boot into pyOS 20.69? (Y)es/(N)o: ")
+ cbt = input("Boot into pyOS 20.69? [Y/n]: ")
  cbt=cbt.upper()
  if cbt == "Y":
     print("Initiating boot sequence")
