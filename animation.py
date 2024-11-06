827768e10a0bd77c6eb447261845419037b59c02
senhas-rgb <senhasdiwan@gmail.com>
2024-11-06 18:03:41 +0530

    Enhancements

--- animation.py
+++ animation.py
@@ -1,14 +1,28 @@
 # Animation
 #!/usr/bin/env python
 import time
-def progress(percent=0, width=20):
-    # The number of hashes to show is based on the percent passed in. The
-    # number of blanks is whatever space is left after.
-    hashes = width * percent // 100
-    blanks = width - hashes
+import sys
+import itertools
+import threading
+
+def progress(percent=0, width=30, message="Loading"):
+    """Enhanced progress bar with message and percentage"""
+    filled = width * percent // 100
+    bar = '█' * filled + '░' * (width - filled)
+    print(f'\r{message}: |{bar}| {percent:.0f}%', end='', flush=True)
+
+def progress_spinner(duration, message="Processing"):
+    """New spinning cursor animation"""
+    spinner = itertools.cycle(['⠋','⠙','⠹','⠸','⠼','⠴','⠦','⠧','⠇','⠏'])
+    end_time = time.time() + duration
     
-    print('\r[', hashes*'#', blanks*' ', ']', f' {percent:.0f}%', sep='',
-        end='\r', flush=True)
+    while time.time() < end_time:
+        sys.stdout.write(f'\r{message} {next(spinner)} ')
+        sys.stdout.flush()
+        time.sleep(0.1)
+    
+    sys.stdout.write('\r' + ' ' * (len(message) + 2) + '\r')
+    sys.stdout.flush()
 
 #Use the following code to use the animation
 #for i in range(101):
