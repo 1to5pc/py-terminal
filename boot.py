eb0e3947ec4ac581c0cdebbec5edd6750307ea67
ravinu <github@swore3556.anonaddy.me>
2024-11-14 06:42:23 +0530

    Catch exception (Ctrl + C)

--- boot.py
+++ boot.py
@@ -12,14 +12,17 @@ def boot():
     animation.progress_spinner(1, "Initializing system")
     
     while True:
-        cbt = input(f"\n{Fore.YELLOW}Boot into pyOS 20.69? [Y/n]:{Style.RESET_ALL} ").lower()
+        try:
+            cbt = input(f"\n{Fore.YELLOW}Boot into pyOS 20.69? [Y/n]:{Style.RESET_ALL} ").lower()
+        except KeyboardInterrupt:
+            cbt='n'
         if cbt in ['y', 'yes', '']:
             print(f"\n{Fore.GREEN}[OK]{Style.RESET_ALL} Initiating boot sequence")
             animation.progress(100, message="Loading system components")
             print()  # New line after progress bar
             return
         elif cbt in ['n', 'no']:
-            print(f"\n{Fore.RED}[ABORT]{Style.RESET_ALL} Boot sequence cancelled")
+            print(f"\n{Fore.RED}[ABORT]{Style.RESET_ALL} Boot sequence aborted")
             exit()
         else:
             print(f"{Fore.RED}Invalid input. Please enter Y or N{Style.RESET_ALL}")
