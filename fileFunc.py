d55f7b7b23f10374829b19331f64db91b81d7357
ravinu <github@swore3556.anonaddy.me>
2024-11-14 11:03:42 +0530

    Add directory list func and file write func

---
+++ fileFunc.py
@@ -0,0 +1,22 @@
+def ls(directory='.'):
+    import os
+    if os.path.isdir(directory):
+        file_names = list(map(lambda file: file, filter(lambda f: os.path.isfile(os.path.join(directory, f)), os.listdir(directory))))
+        return 0, file_names
+    return 1, []
+print(ls())
+
+def fwrite(fName,text):
+#    import os
+#    if os.path.isfile(fName):
+    try:
+        file = open(fName, 'w')
+        try:
+            file.write(text)
+            return 0, ''
+        except:
+            return -1, 'Error writing to file'
+    except:
+        return -1, 'Error opening file'
+    
+    file.close()
