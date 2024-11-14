c09d2fcff567765ffce5e4898ea22f8a03cfdd36
ravinu <github@swore3556.anonaddy.me>
2024-11-14 12:25:48 +0530

    Add dir listing and write to file functionality

--- terminalx.py
+++ terminalx.py
@@ -2,6 +2,8 @@ import time
 import os
 import animation
 import boot
+import fileFunc
+import llmAPI
 from colorama import Fore, Style, init  # For cross-platform colored output
 
 # Initialize colorama
@@ -10,6 +12,8 @@ init()
 # Dictionary of available commands for easier maintenance
 COMMANDS = {
     'py': {'desc': 'Enter a Python environment', 'color': Fore.BLUE},
+    'fwrite': {'desc': 'Write user input to a configurable file', 'color': Fore.BLUE},
+    'ls': {'desc': 'List files in the working directory', 'color': Fore.BLUE},
     'help': {'desc': 'Show the help text for Terminalx', 'color': Fore.GREEN},
     'exit': {'desc': 'Exit Terminalx', 'color': Fore.RED},
     'clear': {'desc': 'Clear the terminal screen', 'color': Fore.CYAN},
@@ -20,9 +24,9 @@ def clear_screen(slptime=0):
         time.sleep(slptime)
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def prgmExit():
+def prgmExit(): #TODO reduce exit time
     print(f"{Fore.YELLOW}Exiting Terminalx...{Style.RESET_ALL}")
-    animation.progress(100, message="Shutting down")
+    animation.progress(AnimTime=0.4, message="Shutting down")
     print(f"\n{Fore.GREEN}[OK]{Style.RESET_ALL} Quit 'Terminalx'")
 
 def main():
@@ -53,7 +57,19 @@ def main():
                 for command, details in COMMANDS.items():
                     print(f"{details['color']}{command:<10}{Style.RESET_ALL} - {details['desc']}")
                 print()
-                
+            elif cmd == 'ls':
+                errCode,dirlist=fileFunc.ls()
+                if errCode == 0:
+                    list(map(print, dirlist))
+                else:
+                    print(f"{Fore.RED}[ERROR]{Style.RESET_ALL} The command failed to run")
+            elif cmd == 'fwrite':
+                usrInput=input("Enter the text you want to write and the file name in natural langauge:\n")
+                errCode,fileName,inputText = llmAPI.NatLangParser(usrInput)
+                if errCode == 0:
+                    errCode, errTxt =fileFunc.fwrite(fileName, inputText)
+                    if errCode != 0:
+                        print(f"{Fore.RED}[ERROR] {Style.RESET_ALL}" + errTxt)
             else:
                 print(f"{Fore.RED}Unknown command!{Style.RESET_ALL} Run 'help' for available commands")
                 
